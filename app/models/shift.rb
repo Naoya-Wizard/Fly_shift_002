e14fe08ccf3d64f2dff4a0ede0e93da603f372d7
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-04-03 19:51:52 +0900

    アクティブハッシュ導入

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,6 @@
 class Shift < ApplicationRecord
-    
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :month
+    belongs_to :user
+
 end
--- db/migrate/20210403053302_create_shifts.rb
+++ db/migrate/20210403053302_create_shifts.rb
@@ -3,6 +3,7 @@ class CreateShifts < ActiveRecord::Migration[6.0]
     create_table :shifts do |t|
       t.text :shift
       t.integer :month_id
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
