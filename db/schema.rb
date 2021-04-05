2cc23346612c11351cd0bae56e1b2e8d186f1747
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-04-05 23:22:20 +0900

    listsコントローラーとモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_090057) do
+ActiveRecord::Schema.define(version: 2021_04_05_141009) do
+
+  create_table "lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "list"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
 
   create_table "shifts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "shift"
@@ -35,5 +43,6 @@ ActiveRecord::Schema.define(version: 2021_04_03_090057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lists", "users"
   add_foreign_key "shifts", "users"
 end
