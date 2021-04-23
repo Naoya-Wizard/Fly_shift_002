4279279b36555e7f48dc3d01b1f1112ac7513c10
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-04-23 11:56:41 +0900

    付箋のバックグラウンドが出るように実装

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -2,8 +2,8 @@ var select = document.getElementById( 'month-category' );
 select.onchange = function(){
     document.getElementById("template").hidden = false;
     document.getElementById("speak_shift").hidden = false;
-    document.getElementById("speak_shift").textContent = `シフトを入力し追加してください!`;
-    document.getElementById("shift_list").innerHTML = '<input type="text" name="name" id="list_input"><button id="add_button" onclick="buttonClick()">追加</button>'
+    document.getElementById("speak_shift").textContent = `シフトを1つずつ入力し、追加してください!`;
+    document.getElementById("shift_list").innerHTML = '<input type="text" name="name" id="list_input" placeholder="例）日勤"><button id="add_button" onclick="buttonClick()">追加</button>'
     document.getElementById("shift_list").insertAdjacentHTML('beforeEnd', ` <button class="button" id="button" onclick="calender()">終了</button>`);
 };
 
